99926a37f0cf0ee652872762508a1aa2c22fcd38
arielle_izaa <105427093+apequenagrandeizadora@users.noreply.github.com>
2023-10-23 10:46:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function tocasomabelha(){
+    document.querySelector("#som_tecla_abelha").play();
+}
+const litadeTeclas = document.querySelectorAll(".tecla");
+
+listadeTeclas[2].onclick = tocasomabelha
